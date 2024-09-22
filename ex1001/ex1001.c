bc4e4afea9a6d77c667cd3d74a0efb5d055ad960
JoaoVictorSicco <victordesicco@gmail.com>
2024-09-21 21:37:17 -0500

    Problema 1001: Extremamente básico

---
+++ ex1001/ex1001.c
@@ -0,0 +1,14 @@
+/*Problema 1001: Extremamente básico*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int A,B, soma;
+    scanf("%d %d", &A, &B);
+    soma = A + B;
+    printf("X = %d\n", soma);
+    return 0;
+    
+}
