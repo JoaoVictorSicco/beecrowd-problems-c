8ce494e6b06062821ceb0d0db0b1c59ec1659d0f
JoaoVictorSicco <victordesicco@gmail.com>
2024-09-22 11:47:39 -0500

    Problema 1004: Produto Simples

---
+++ ex1004/ex1004.c
@@ -0,0 +1,12 @@
+/* Problema 1004: Produto Simples */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int A, B, PROD;
+    scanf("%d %d", &A, &B);
+    PROD = A*B;
+    printf("PROD = %d\n", PROD);
+}
